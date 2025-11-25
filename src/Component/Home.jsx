713ebe7d34eb8227f58768b66748b9d2b87c1a47
Divya1512004 <mayadivya39@gmail.com>
2025-11-25 20:30:40 +0530

    modified app.css home.css home.jsx header.css

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -6,15 +6,11 @@ function Home() {
   return (
     <div className='home'>
         <div className="home-image">
-          {/* <div className="content"> */}
             <h1>WELCOME TO OUR TRESSY🛍️👗</h1>
-            {/* <div className="content1"> */}
               <p>Discover The Latest Trends And Styles With Us!</p>
               <p>Find Trendy, Classy And Comfy Styles All In One Place.🤩💃🛒</p>
               <p>Shop Your Favorites, AnyTime, AnyWhere With Just a Tap💗🌸👜</p>
-            {/* </div> */}
             <h2>YOUR PERFECT DRESS IS WAITING - START YOUR STYLISH JOURNEY NOW!👚👠👒</ h2>
-          {/* </div> */}
         </div>
         <div className="section">
             <Section />
